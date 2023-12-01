4268f233e59725cbd35ab502cea3c3c849fb0a07
Kamil <kamil.p@devpark.pl>
2023-12-01 08:41:26 +0100

    Number Of One Bits Finder

--- src/app/NumberOfOneBits/NumberOfOneBitsFinder.php
+++ src/app/NumberOfOneBits/NumberOfOneBitsFinder.php
@@ -7,6 +7,14 @@ class NumberOfOneBitsFinder
 {
     public function search(int $bit_string):int
     {
-
+        //00000000000000000000000000001011
+        $sum = 0;
+        $bit_string = decbin($bit_string);
+        for ($i = 0;$i<strlen($bit_string);$i++){
+            if($bit_string[$i]){
+                $sum++;
+            }
+        }
+        return $sum;
     }
 }
--- src/tests/Unit/NumberOfOneBitsFinder/NumberOfOneBitsFinderTest.php
+++ src/tests/Unit/NumberOfOneBitsFinder/NumberOfOneBitsFinderTest.php
@@ -10,14 +10,32 @@ use PHPUnit\Framework\TestCase;
 class NumberOfOneBitsFinderTest extends TestCase
 {
     /**
-     *  @data-provider
+     *  @dataProvider  bitStringProvider
      */
     public function test_find_number_of_one_bits(int $bits_string, int $one_count){
 
-        $number_of_the_bits = new NumberOfOneBitsFinder();
-        $number_of_the_bits->search($bits_string);
+        $finder = new NumberOfOneBitsFinder();
+        $number_of_the_bits = $finder->search( $bits_string);
 
-        $this->
+        $this->assertSame($one_count, $number_of_the_bits);
 
+    }
+
+    public function bitStringProvider():array{
+        return [
+            [
+                'bits_string' => 11,
+                'one_count' => 3,
+            ],
+            [
+                'bits_string' => 2097152,
+                'one_count' => 1,
+            ],
+            [
+                'bits_string' => 4294967293,
+                'one_count' => 31,
+            ],
+
+        ];
     }
 }
